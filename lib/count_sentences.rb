de16dae33953a5edc3ec4d211b96e96d97e9cb1f
Eric <eric.yanez50@qmail.cuny.edu>
2022-08-10 10:48:34 -0400

    commit tree 3d49db2fb45c7925ff0b38d128ca7ef01b254bf6

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # if self.end_with?(".")
+    #   return true
+    # else
+    #   false
+    # end
+    self.end_with?(".") ? true : false
+    # both solutions work, however I went with a simple clean ternary
+  
   end
 
   def question?
+    self.end_with?("?") ? true : false
 
   end
 
   def exclamation?
+    self.end_with?("!") ? true : false
 
   end
 
   def count_sentences
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
 
   end
 end
